74adb415f0adaa2bc439db52cf022c2c60f59317
pandapan0021 <13553023182@163.com>
2016-10-24 18:41:27 +0800

    Create 1_3.py

---
+++ chapter01/1_3.py
@@ -0,0 +1,19 @@
+def max_in_three(first, second, three):
+    """
+    return the max num in three args
+    """
+    
+    f = lambda x, y: x if x>y else y
+    
+    return f(f(first, second), three)
+    
+    
+#使用functools.reduce可以得到任意个参数最大值
+
+import functools.reduce
+
+def max_items(*args):
+    """
+    find max in unknown args
+    """
+    return reduce((lambda x, y: x if x>y else y), args)
